4e2086efb8d1964d4e7efcf9dcefb653ecc3dc97
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2024-01-15 17:59:42 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;                           // A Simple Table to Print
+        for (int i = 0;i<=10;i++){
+            System.out.println(a +" * "+i+" = "+a*i);
+        }
+    }
+}
