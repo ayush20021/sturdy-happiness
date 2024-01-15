2962cc754a722aa6d2a92badeda3e0a051c5b1da
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2024-01-15 18:06:11 +0530

    First Commit

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         for (int i = 0;i<=10;i++){
             System.out.println(a +" * "+i+" = "+a*i);
             System.out.println("OK");
+            System.out.println("3");
         }
     }
 }
